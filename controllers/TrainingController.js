f3e03273a09341858577e864369c04a1418ebfce
Jacob Boyles <fixterjake@gmail.com>
2023-09-27 00:27:33 -0400

    log request body

--- controllers/TrainingController.js
+++ controllers/TrainingController.js
@@ -455,6 +455,7 @@ router.put('/session/save/:id', getUser, auth(['atm', 'datm', 'ta', 'ata', 'ins'
 
 router.post('/session/new', getUser, auth(['atm', 'datm', 'ta', 'ata', 'ins', 'mtr']), async (req, res) => {
 	try {
+		console.log(req.body);
 		if(!req.body.studentCid || !req.body.startTime || !req.body.endTime || !req.body.milestone || !req.body.insNotes ||
 			!req.body.position || !req.body.progress || !req.body.movements || !req.body.location ||
 			!req.body.ots || !req.body.studentNotes) {
